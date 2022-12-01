b69a63332327c17135a4d641fc48fb427b9411cb
TiavinaTiTi <titus50231@gmail.com>
2022-12-01 16:25:20 +0300

    User ChartJS in Angular 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphComponent } from './features/graph/graph.component';
+import { BarGraphComponent } from './features/bar-graph/bar-graph.component';
+import { LineGraphComponent } from './features/line-graph/line-graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphComponent,
+    BarGraphComponent,
+    LineGraphComponent
   ],
   imports: [
     BrowserModule,
